241c05fb6b04330801dbf74518fd2e21be83238d
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-01 05:14:14 +0000

    Day 1. Part 2. 2059.

--- 1/run.php
+++ 1/run.php
@@ -8,19 +8,22 @@
 	foreach ($input as $line) {
 		preg_match('#(.*) (.*)#SADi', $line, $m);
 		[$all, $foo, $bar] = $m;
-		$list1[] = $foo;
-		$list2[] = $bar;
+		$list1[] = trim($foo);
+		$list2[] = trim($bar);
 	}
 
 	sort($list1);
 	sort($list2);
 
+	$list2acv = array_count_values($list2);
+
 	$part1 = 0;
+	$part2 = 0;
 	for ($i = 0; $i < count($list1); $i++) {
 		$part1 += abs($list1[$i] - $list2[$i]);
+		$part2 += ($list2acv[$list1[$i]] ?? 0) * $list1[$i];
 	}
 
 	echo 'Part 1: ', $part1, "\n";
 
-	// $part2 = 0;
-	// echo 'Part 2: ', $part2, "\n";
+	echo 'Part 2: ', $part2, "\n";
