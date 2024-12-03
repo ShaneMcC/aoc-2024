988f3d71800b0cb49320f225f385f3b33747f6e0
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-03 05:15:22 +0000

    Day 3. Part 2. 1801

--- 3/run.php
+++ 3/run.php
@@ -5,18 +5,27 @@
 
 	$entries = [];
 	foreach ($input as $line) {
-		preg_match_all('#mul\(([0-9]{1,3},[0-9]{1,3})\)#', $line, $m);
+		preg_match_all("#(do\(\)|don't\(\)|mul\(([0-9]{1,3},[0-9]{1,3})\))#", $line, $m);
 		foreach ($m[1] as $m) {
 			$entries[] = $m;
 		}
 	}
 
 	$part1 = 0;
+	$part2 = 0;
+	$on = true;
 	foreach ($entries as $muls) {
-		$bits = explode(',', $muls);
-		$part1 += ($bits[0] * $bits[1]);
+		if ($muls == 'do()') {
+			$on = true;
+		} else if ($muls == "don't()") {
+			$on = false;
+		} else {
+			preg_match('#mul\(([0-9]{1,3}),([0-9]{1,3})\)#', $muls, $bits);
+			$sum = ($bits[1] * $bits[2]);
+			$part1 += $sum;
+			$part2 += $on ? $sum : 0;
+		}
 	}
-	echo 'Part 1: ', $part1, "\n";
 
-	// $part2 = 0;
-	// echo 'Part 2: ', $part2, "\n";
+	echo 'Part 1: ', $part1, "\n";
+	echo 'Part 2: ', $part2, "\n";
