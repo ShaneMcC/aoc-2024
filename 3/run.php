53d1f7f8852cd0f81fea931fefad0e6af9eb37c9
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-03 05:37:47 +0000

    Tidy up.

--- 3/run.php
+++ 3/run.php
@@ -5,23 +5,21 @@
 
 	$entries = [];
 	foreach ($input as $line) {
-		preg_match_all("#(do\(\)|don't\(\)|mul\(([0-9]{1,3},[0-9]{1,3})\))#", $line, $m);
-		foreach ($m[1] as $m) {
-			$entries[] = $m;
+		preg_match_all("#(do\(\)|don't\(\)|mul\(([0-9]{1,3}),([0-9]{1,3})\))#", $line, $m);
+		for ($i = 0; $i < count($m[0]); $i++) {
+			$entries[] = empty($m[2][$i]) ? $m[1][$i] : [$m[2][$i], $m[3][$i]];
 		}
 	}
 
-	$part1 = 0;
-	$part2 = 0;
+	$part1 = $part2 = 0;
 	$on = true;
-	foreach ($entries as $muls) {
-		if ($muls == 'do()') {
+	foreach ($entries as $entry) {
+		if ($entry == 'do()') {
 			$on = true;
-		} else if ($muls == "don't()") {
+		} else if ($entry == "don't()") {
 			$on = false;
 		} else {
-			preg_match('#mul\(([0-9]{1,3}),([0-9]{1,3})\)#', $muls, $bits);
-			$sum = ($bits[1] * $bits[2]);
+			$sum = array_product($entry);
 			$part1 += $sum;
 			$part2 += $on ? $sum : 0;
 		}
