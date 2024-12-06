42ebbae0fe4d2b76fe2427a61203f5ec91e05574
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-06 06:17:08 +0000

    Some speed improvements.

--- 6/run.php
+++ 6/run.php
@@ -16,13 +16,13 @@
 		global $directions;
 
 		$isLoop = false;
-		$previous = [];
+		$previousState = [];
+		$visited = [];
 		while (true) {
 			[$x, $y, $face] = $guard;
 			[$dx, $dy, $nextFace] = $directions[$face];
 
-			$map[$y][$x] = 'x';
-			$thisPos = "{$x},{$y},{$face}";
+			$visited["{$x},{$y}"] = true;
 
 			if (!isset($map[$y + $dy][$x + $dx])) {
 				$isLoop = false;
@@ -30,36 +30,35 @@
 			}
 			$next = $map[$y + $dy][$x + $dx];
 
-			if (isset($previous[$thisPos])) {
-				$isLoop = true;
-				break;
-			}
-
-			$previous[$thisPos] = True;
-
 			if ($next == '#' || $next == 'O') {
 				$guard = [$x, $y, $nextFace];
 			} else {
 				$guard = [$x + $dx, $y + $dy, $face];
 			}
+
+			$thisState = "{$x},{$y},{$face}";
+			if (isset($previousState[$thisState])) {
+				$isLoop = true;
+				break;
+			}
+			$previousState[$thisState] = True;
 		}
 
-		return [$map, $isLoop];
+		return [$visited, $isLoop];
 	}
 
 	$visitedMap = getVisitedPositions($map, $guard)[0];
-	$part1 = count(findCells($visitedMap, 'x'));
+	$part1 = count($visitedMap);
 	echo 'Part 1: ', $part1, "\n";
 
 	$part2 = 0;
-	foreach (cells($visitedMap) as [$x, $y, $cell]) {
-		if ($cell == 'x') {
-			$newMap = $map;
-			$newMap[$y][$x] = 'O';
-			[$visitedMap, $looped] = getVisitedPositions($newMap, $guard);
-			if ($looped) {
-				$part2++;
-			}
+	foreach ($visitedMap as $v => $_) {
+		[$x, $y] = explode(',', $v, 2);
+		$newMap = $map;
+		$newMap[$y][$x] = 'O';
+		[$visitedMap, $looped] = getVisitedPositions($newMap, $guard);
+		if ($looped) {
+			$part2++;
 		}
 	}
 	echo 'Part 2: ', $part2, "\n";
