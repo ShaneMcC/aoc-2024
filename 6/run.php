10d6c1c1a8bd05b9c7e1f01f13c78c510fc8a30a
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-06 06:00:12 +0000

    Day 6. Part 2. 3428. I spent too long on this because I overcomplicated it for no reason.

--- 6/run.php
+++ 6/run.php
@@ -15,29 +15,51 @@
 	function getVisitedPositions($map, $guard) {
 		global $directions;
 
-		$count = 0;
+		$isLoop = false;
+		$previous = [];
 		while (true) {
 			[$x, $y, $face] = $guard;
 			[$dx, $dy, $nextFace] = $directions[$face];
 
 			$map[$y][$x] = 'x';
+			$thisPos = "{$x},{$y},{$face}";
 
-			if (!isset($map[$y + $dy][$x + $dx])) { break; }
+			if (!isset($map[$y + $dy][$x + $dx])) {
+				$isLoop = false;
+				break;
+			}
 			$next = $map[$y + $dy][$x + $dx];
 
-			if ($next == '#') {
+			if (isset($previous[$thisPos])) {
+				$isLoop = true;
+				break;
+			}
+
+			$previous[$thisPos] = True;
+
+			if ($next == '#' || $next == 'O') {
 				$guard = [$x, $y, $nextFace];
 			} else {
 				$guard = [$x + $dx, $y + $dy, $face];
 			}
 		}
 
-		return findCells($map, 'x');
+		return [$map, $isLoop];
 	}
 
-	$visitedMap = getVisitedPositions($map, $guard);
-	$part1 = count($visitedMap);
+	$visitedMap = getVisitedPositions($map, $guard)[0];
+	$part1 = count(findCells($visitedMap, 'x'));
 	echo 'Part 1: ', $part1, "\n";
 
-	// $part2 = 0;
-	// echo 'Part 2: ', $part2, "\n";
+	$part2 = 0;
+	foreach (cells($visitedMap) as [$x, $y, $cell]) {
+		if ($cell == 'x') {
+			$newMap = $map;
+			$newMap[$y][$x] = 'O';
+			[$visitedMap, $looped] = getVisitedPositions($newMap, $guard);
+			if ($looped) {
+				$part2++;
+			}
+		}
+	}
+	echo 'Part 2: ', $part2, "\n";
