4e686395f433fff7615b32d78e3bd984d4662d35
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-07 05:13:31 +0000

    Day 7. Part 2. 1186

--- 7/run.php
+++ 7/run.php
@@ -10,7 +10,7 @@
 		$entries[$foo] = explode(' ', $bar);
 	}
 
-	function checkValid($score, $values) {
+	function checkValid($score, $values, $allowConcat = false) {
 		$attempts = [null];
 		foreach ($values as $v) {
 			$newAttempts = [];
@@ -20,6 +20,11 @@
 				$a2 = ($a === null ? 0 : $a) + $v;
 				if ($a1 <= $score) { $newAttempts[] = $a1; }
 				if ($a2 <= $score) { $newAttempts[] = $a2; }
+
+				if ($allowConcat) {
+					$a3 = ($a === null ? '' : $a) . $v;
+					if ($a3 <= $score) { $newAttempts[] = $a3; }
+				}
 			}
 
 			$attempts = $newAttempts;
@@ -37,12 +42,15 @@
 	$part1 = $part2 = 0;
 
 	foreach ($entries as $score => $values) {
-		// echo $score, ': ', json_encode($values), ' => ', checkValid($score, $values), "\n";
+		// echo $score, ': ', json_encode($values), ' => ', checkValid($score, $values, true), "\n";
 
 		if (checkValid($score, $values)) {
 			$part1 += $score;
+			$part2 += $score;
+		} else if (checkValid($score, $values, true)) {
+			$part2 += $score;
 		}
 	}
 
 	echo 'Part 1: ', $part1, "\n";
-	// echo 'Part 2: ', $part2, "\n";
+	echo 'Part 2: ', $part2, "\n";
