8a55bf9c192ee6f46d9abfa17ee5b98c4e6443ad
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-11 05:20:37 +0000

    Tidying up a bit.

--- 11/run.php
+++ 11/run.php
@@ -1,8 +1,7 @@
 #!/usr/bin/php
 <?php
 	require_once(dirname(__FILE__) . '/../common/common.php');
-	$input = explode(' ', getInputLine());
-	$input = array_map(fn($x) => intval(($x)), $input);
+	$input = array_map(fn($x) => (int)$x, explode(' ', getInputLine()));
 
 	function blinkCount($stone, $count) {
 		$key = json_encode([__FILE__, __LINE__, func_get_args()]);
@@ -10,16 +9,20 @@
 		return storeCachedResult($key, function() use ($stone, $count) {
 			if ($count == 0) {
 				return 1;
-			} if ($stone === 0) {
+			}
+
+			if ($stone === 0) {
 				return blinkCount(1, $count - 1);
-			} else if (strlen($stone) % 2 == 0) {
-				$left = intval(substr($stone, 0, strlen($stone) / 2));
-				$right = intval(substr($stone, strlen($stone) / 2));
+			}
+
+			if (strlen($stone) % 2 == 0) {
+				$left = (int) substr($stone, 0, strlen($stone) / 2);
+				$right = (int) substr($stone, strlen($stone) / 2);
 
 				return blinkCount($left, $count - 1) + blinkCount($right, $count - 1);
-			} else {
-				return blinkCount($stone * 2024, $count - 1);
 			}
+
+			return blinkCount($stone * 2024, $count - 1);
 		});
 	}
 
