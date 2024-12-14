a5b343929c8fbd06b71ef80e568f9f12e8af1367
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-14 06:14:37 +0000

    Day 14. Part 2. 1046

--- 14/run.php
+++ 14/run.php
@@ -21,8 +21,6 @@
 		$px += ($vx * $seconds);
 		$py += ($vy * $seconds);
 
-
-
 		if ($px > $mapMaxX) { $px = $px % $mapMaxX; }
 		if ($py > $mapMaxY) { $py = $py % $mapMaxY; }
 
@@ -32,40 +30,52 @@
 		return [$px, $py];
 	}
 
-	if (isDebug()) {
-		$map = array_fill(0, $mapMaxY, array_fill(0, $mapMaxX, '.'));
-		drawMap($map, true);
-	}
-
-	$q0 = 0;
-	$q1 = 0;
-	$q2 = 0;
-	$q3 = 0;
+	$q0 = $q1 = $q2 = $q3 = 0;
 
 	$halfX = (int)floor($mapMaxX / 2);
 	$halfY = (int)floor($mapMaxY / 2);
 
-	foreach ($robots as $robot) {
-		[$px, $py] = getRobotPosition($robot, 100);
+	$part1 = $part2 = 0;
 
-		if ($px < $halfX && $py < $halfY) { $q0++; }
-		if ($px < $halfX && $py > $halfY) { $q1++; }
-		if ($px > $halfX && $py < $halfY) { $q2++; }
-		if ($px > $halfX && $py > $halfY) { $q3++; }
+	for ($i = 0; $i < 10000; $i++) {
+		$map = array_fill(0, $mapMaxY+1, array_fill(0, $mapMaxX+1, null));
+
+		// Secret property of the inputs...
+		// The easter-egg is the first frame with no overlaps.
+		// I hate it.
+		$hasOverlaps = false;
+		foreach ($robots as $robot) {
+			[$px, $py] = getRobotPosition($robot, $i);
+
+			if ($i == 100) {
+				if ($px < $halfX && $py < $halfY) { $q0++; }
+				if ($px < $halfX && $py > $halfY) { $q1++; }
+				if ($px > $halfX && $py < $halfY) { $q2++; }
+				if ($px > $halfX && $py > $halfY) { $q3++; }
+			}
+
+			$map[$py][$px]++;
+			if ($map[$py][$px] > 1) {
+				$hasOverlaps = True;
+				if ($i != 100 && !isDebug()) {
+					break;
+				}
+			}
+		}
 
 		if (isDebug()) {
-			if ($map[$py][$px] == '.') { $map[$py][$px] = 0; }
-			$map[$py][$px]++;
+			drawSparseMap($map, '.', true, $i);
+		}
+
+		if ($i == 100) {
+			$part1 = $q0 * $q1 * $q2 * $q3;
+			echo 'Part 1: ', $part1, "\n";
+		}
+
+		if (!$hasOverlaps) {
+			$part2 = $i;
+			break;
 		}
 	}
 
-	if (isDebug()) { drawMap($map, false); }
-
-	$part1 = $q0 * $q1 * $q2 * $q3;
-
-	// foreach
-
-	echo 'Part 1: ', $part1, "\n";
-
-	// $part2 = 0;
-	// echo 'Part 2: ', $part2, "\n";
+	echo 'Part 2: ', $part2, "\n";
