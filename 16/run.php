a7c7726e94d80ae61d5fcc52f9c2ee5f2035103e
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-16 06:47:01 +0000

    Day 16. Part 2. 2980

--- 16/run.php
+++ 16/run.php
@@ -4,31 +4,13 @@
 	$map = getInputMap();
 
 	$directions = [];
-	$directions['N'] = [0, -1, ['W', 'E'], '^'];
-	$directions['E'] = [1, 0, ['N', 'S'], '>'];
-	$directions['S'] = [0, 1, ['E', 'W'], 'v'];
-	$directions['W'] = [-1, 0, ['S', 'N'], '<'];
+	$directions['N'] = [0, -1, ['W', 'E']];
+	$directions['E'] = [1, 0, ['N', 'S']];
+	$directions['S'] = [0, 1, ['E', 'W']];
+	$directions['W'] = [-1, 0, ['S', 'N']];
 
 	$start = findCells($map, 'S')[0];
 	$end = findCells($map, 'E')[0];
-
-    function fluffMap($map, $title = '') {
-        foreach (cells($map) as [$x, $y, $cell]) {
-            if ($cell == '#') {
-                $map[$y][$x] = "\033[91m" . $cell . "\033[0m";
-            }
-
-            if ($cell == '.') {
-                $map[$y][$x] = ' ';
-            }
-
-            if ($cell == '^' || $cell == '>' || $cell == 'v' || $cell == '<') {
-                $map[$y][$x] = "\033[92m" . $cell . "\033[0m";
-            }
-        }
-        drawMap($map, false, $title);
-    }
-
 
 	function getPathCost($map, $start, $end) {
 		global $directions;
@@ -37,37 +19,44 @@
 		$queue->setExtractFlags(SplPriorityQueue::EXTR_BOTH);
 		$queue->insert([$start[0], $start[1], 'E', []], 0);
 
+		$locations = [];
 		$costs = [];
+
+		$maxCost = PHP_INT_MAX;
 
 		while (!$queue->isEmpty()) {
 			$q = $queue->extract();
 			[$x, $y, $direction, $steps] = $q['data'];
 			$cost = abs($q['priority']);
 
-			if (isset($costs[$y][$x][$direction])) { continue; }
-			$costs[$y][$x][$direction] = $cost;
+			$costs[$y][$x][$direction] = min($cost, $costs[$y][$x][$direction] ?? PHP_INT_MAX);
+			if ($cost > $costs[$y][$x][$direction]) { continue; }
 
-			[$dX, $dY, $possibleDirections, $shape] = $directions[$direction];
+			if ([$x, $y] == $end) {
+				if ($cost < $maxCost) {
+					$maxCost = min($cost, $maxCost);
+					$locations = [];
+				}
+
+				if ($cost == $maxCost) {
+					$locations = array_unique(array_merge($locations, $steps));
+				}
+			}
+
+			[$dX, $dY, $possibleDirections] = $directions[$direction];
 			[$tX, $tY] = [$x + $dX, $y + $dY];
 
 			if ($map[$tY][$tX] != '#') {
-				$queue->insert([$tX, $tY, $direction, array_merge($steps, [[$x, $y, $shape]])], -($cost + 1));
+				$queue->insert([$tX, $tY, $direction, array_merge($steps, ["{$x},{$y}"])], -($cost + 1));
 			}
 			foreach ($possibleDirections as $pd) {
 				$queue->insert([$x, $y, $pd, $steps], -($cost + 1000));
 			}
 		}
 
-		[$x, $y] = $end;
-		if (isset($costs[$y][$x])) {
-			return min(array_values($costs[$y][$x]));
-		}
-
-		return PHP_INT_MAX;
+		return [$maxCost, count($locations) + 1];
 	}
 
-	$part1 = getPathCost($map, $start, $end);
+	[$part1, $part2] = getPathCost($map, $start, $end);
 	echo 'Part 1: ', $part1, "\n";
-
-	// $part2 = 0;
-	// echo 'Part 2: ', $part2, "\n";
+	echo 'Part 2: ', $part2, "\n";
