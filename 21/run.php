b4d7fe3bacd8ae5199d8e85a760f521ed9d8fd87
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-21 08:31:40 +0000

    Speed up part 1.

--- 21/run.php
+++ 21/run.php
@@ -6,155 +6,81 @@
 	$mainKeypad = [[7, 8, 9],
 	               [4, 5, 6],
 	               [1, 2, 3],
-	               [null, 0, 'A']];
+	               ['#', 0, 'A']];
 
-    $arrowsPad = [[null, '^', 'A'],
+    $arrowsPad = [['#', '^', 'A'],
 	              ['<', 'v', '>']];
 
-	  function getPaths($map, $start, $end) {
-		$queue = new SPLPriorityQueue();
-		$queue->setExtractFlags(SplPriorityQueue::EXTR_BOTH);
-		$queue->insert([$start[0], $start[1], [], []], 0);
+	function getPathFromAtoB($keypad, $start, $end) {
+		if ($start == $end) { return ''; }
 
-		$adj = [];
-		$adj['^'] = [0, -1];
-		$adj['<'] = [-1, 0];
-		$adj['>'] = [1, 0];
-		$adj['v'] = [0, 1];
+		[$sX, $sY] = findCells($keypad, $start)[0];
+		[$eX, $eY] = findCells($keypad, $end)[0];
 
-		$paths = [];
+		[$dX, $dY] = [$eX - $sX, $eY - $sY];
 
-		$bestLen = PHP_INT_MAX;
+		$vdir = $hdir = '';
 
-		while (!$queue->isEmpty()) {
-			$q = $queue->extract();
-			[$x, $y, $dirs, $prev] = $q['data'];
-			$cost = abs($q['priority']);
-
-			if (isset($prev[$y][$x])) { continue; }
-			$prev[$y][$x] = True;
-
-			$myLen = count($dirs);
-			if ($myLen > $bestLen) { continue; }
-
-			if ([$x, $y] == $end) {
-				if ($myLen < $bestLen) {
-					$paths = [];
-				}
-				$bestLen = min($bestLen, $myLen);
-				$paths[] = $dirs;
-				continue;
-			}
-
-			foreach ($adj as $dir => [$dX, $dY]) {
-				[$tX, $tY] = [$x + $dX, $y + $dY];
-				if (isset($map[$tY][$tX]) && $map[$tY][$tX] !== null) {
-					$queue->insert([$tX, $tY, array_merge($dirs, [$dir]), $prev], -($cost + 1));
-				}
-			}
+		if ($sY > $eY) {
+			$vdir = '^';
+		} else if ($sY < $eY) {
+			$vdir = 'v';
 		}
 
-		return $paths;
+		if ($sX > $eX) {
+			$hdir = '<';
+		} else if ($sX < $eX) {
+			$hdir = '>';
+		}
+
+		$vertical = str_repeat($vdir, abs($dY));
+		$horizontal = str_repeat($hdir, abs($dX));
+
+		if ($hdir == '<' && $keypad[$sY][$eX] != '#') {
+			return $horizontal . $vertical;
+		}
+
+		if ($hdir == '>' || $keypad[$eY][$sX] != '#') {
+			return $vertical . $horizontal;
+		}
+
+		return $horizontal . $vertical;
 	}
 
-	function getPathsFromAtoB($keypad, $start, $end) {
-		return getPaths($keypad, findCells($keypad, $start)[0], findCells($keypad, $end)[0]);
-	}
-
-	function getSequence($keypad, $sequence, $all = False) {
+	function getSequence($keypad, $sequence) {
 		$current = 'A';
 
-		$results = [''];
+		$result = '';
 
 		foreach (str_split($sequence) as $c) {
-			$paths = getPathsFromAtoB($keypad, $current, $c);
-
-			$newResults = [];
-			foreach ($results as $r) {
-				foreach ($paths as $p) {
-					$newResults[] = $r . implode('', $p) . 'A';
-				}
-			}
-			$results = $newResults;
-
+			$path = getPathFromAtoB($keypad, $current, $c);
+			$result .= $path . 'A';
 			$current = $c;
 		}
 
-		$len = PHP_INT_MAX;
-		$path = '';
-		foreach ($results as $r) {
-			if (strlen($r) < $len) {
-				$path = $r;
-				$len = strlen($r);
-			}
-		}
-
-		return $all ? $results : $path;
+		return $result;
 	}
 
-	function getNestedSequence($sequence) {
+	function getNestedSequence($sequence, $chain = 2) {
 		global $arrowsPad, $mainKeypad;
 
-		$possible = [];
+		$last = getSequence($mainKeypad, $sequence);
 
-		$bestLen = PHP_INT_MAX;
-
-		foreach (getSequence($mainKeypad, $sequence, true) as $inner) {
-			foreach (getSequence($arrowsPad, $inner, true) as $mid) {
-				foreach (getSequence($arrowsPad, $mid, true) as $outer) {
-					$myLen = strlen($outer);
-					if ($myLen < $bestLen) {
-						$possible = [];
-						$bestLen = min($bestLen, $myLen);
-					}
-					if ($myLen <= $bestLen) {
-						$possible[] = $outer;
-					}
-				}
-			}
+		for ($i = 0; $i < $chain; $i++) {
+			$last = getSequence($arrowsPad, $last);
 		}
 
-		return $possible[0];
+		return $last;
 	}
-
-	function debugSequence($keypad, $sequence) {
-		$adj = [];
-		$adj['^'] = [0, -1];
-		$adj['<'] = [-1, 0];
-		$adj['>'] = [1, 0];
-		$adj['v'] = [0, 1];
-
-		[$x, $y] = findCells($keypad, 'A')[0];
-		$pressed = '';
-
-		foreach (str_split($sequence) as $c) {
-			if ($c == 'A') {
-				$pressed .= $keypad[$y][$x];
-			} else {
-				[$dX, $dY] = $adj[$c];
-				[$x, $y] = [$x + $dX, $y + $dY];
-			}
-		}
-
-		return $pressed;
-	}
-
-	function debugNestedSequence($sequence) {
-		global $arrowsPad, $mainKeypad;
-
-		return debugSequence($mainKeypad, debugSequence($arrowsPad, debugSequence($arrowsPad, $sequence)));
-	}
-
 
 	$part1 = 0;
 	foreach ($input as $line) {
 		$seq = getNestedSequence($line);
-		$debug = debugNestedSequence($seq);
 		$numVal = (int)str_replace('A', '', $line);
 		$seqLen = strlen($seq);
 		$calc = $numVal * $seqLen;
 
-		echo $line, ': ', $seq, " => {$seqLen} * {$numVal} = {$calc} [{$debug}]", "\n";
+		echo $line, ': ', $seq, " => {$seqLen} * {$numVal} = {$calc}", "\n";
 
 		$part1 += $calc;
 	}
