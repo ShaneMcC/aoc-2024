ef74b75a4c9f5321f247a81a7acf133d12450a6f
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-22 06:11:16 +0000

    Day 22. Part 2. 2552

--- 22/run.php
+++ 22/run.php
@@ -20,14 +20,46 @@
 		return $secret;
 	}
 
-	$part1 = 0;
-	foreach ($input as $num) {
-		$result = $num;
-		for ($i = 0; $i < 2000; $i++) { $result = getNextSecretNumber($result); }
+	$maxCount = 2000;
 
-		$part1+= $result;
-		if (isDebug()) { echo $num, ': ', $result, "\n"; }
+	$monkeys = [];
+	foreach ($input as $m) { $monkeys[$m] = []; }
+
+	$allSetsOfFour = [];
+	$part1 = 0;
+	foreach ($monkeys as $m => $_) {
+		$secret = $m;
+		$bananas = abs($secret % 10);
+
+		$changes = [];
+		for ($i = 0; $i < $maxCount; $i++) {
+			$secret = getNextSecretNumber($secret);
+			$newBananas = abs($secret % 10);
+			$diff = $newBananas - $bananas;
+			$bananas = $newBananas;
+			$changes[] = [$diff, $bananas];
+		}
+
+		$setsOfFour = [];
+		for ($i = 0; $i < count($changes) - 3; $i++) {
+			$test = [$changes[$i][0], $changes[$i + 1][0], $changes[$i + 2][0], $changes[$i + 3][0]];
+			$implode = implode(',', $test);
+
+			if (!isset($setsOfFour[$implode])) {
+				$setsOfFour[$implode] = $changes[$i + 3][1];
+				$allSetsOfFour[$implode] = ($allSetsOfFour[$implode] ?? 0) + $setsOfFour[$implode];
+			}
+		}
+
+		$monkey = [];
+		// $monkey['changes'] = $changes;
+		$monkey['final'] = $secret;
+		$monkey['setsOfFour'] = $setsOfFour;
+		$monkeys[$m] = $monkey;
+
+		$part1 += $secret;
 	}
 	echo 'Part 1: ', $part1, "\n";
 
-	//
+	$part2 = max($allSetsOfFour);
+	echo 'Part 2: ', $part2, "\n";
