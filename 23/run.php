15f094b8d7c4f6d87aab6b62a618c4e7be240828
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-23 06:20:41 +0000

    Day 23. Part 2. 2309.

--- 23/run.php
+++ 23/run.php
@@ -7,35 +7,72 @@
 	foreach ($input as $line) {
 		preg_match('#(.*)-(.*)#ADi', $line, $m);
 		[$all, $a, $b] = $m;
-		if (!isset($computers[$a])) { $computers[$a] = ['name' => $a, 'links' => []]; }
-		if (!isset($computers[$b])) { $computers[$b] = ['name' => $b, 'links' => []]; }
+		if (!isset($computers[$a])) { $computers[$a] = []; }
+		if (!isset($computers[$b])) { $computers[$b] = []; }
 
-		$computers[$a]['links'][] = $b;
-		$computers[$b]['links'][] = $a;
+		$computers[$a][] = $b;
+		$computers[$b][] = $a;
 	}
 
-	$setsOfThree = [];
+	function findSetsOfThree($computers) {
+		$setsOfThree = [];
 
-	foreach ($computers as $a => $adata) {
-		foreach ($adata['links'] as $b) {
-			foreach ($computers[$b]['links'] as $c) {
-				foreach ($computers[$c]['links'] as $d) {
-					if ($d == $a) {
-						$set = [$a, $b, $c];
-						sort($set);
-						$set = implode(',', $set);
+		foreach ($computers as $a => $adata) {
+			foreach ($adata as $b) {
+				foreach ($computers[$b] as $c) {
+					foreach ($computers[$c] as $d) {
+						if ($d == $a) {
+							$set = [$a, $b, $c];
+							sort($set);
+							$set = implode(',', $set);
 
-						$startsWithT = ($a[0] == 't' || $b[0] == 't' || $c[0] == 't');
+							$startsWithT = ($a[0] == 't' || $b[0] == 't' || $c[0] == 't');
 
-						$setsOfThree[$set] = $startsWithT;
+							$setsOfThree[$set] = $startsWithT;
+						}
 					}
 				}
 			}
 		}
+
+		return $setsOfThree;
 	}
 
+	// Bron–Kerbosch algorithm.
+	//
+	// Based on https://stackoverflow.com/a/24415469
+	function findMaximalCliques($computers, $possibleClique=[], $skip=[]) {
+		$cliques = [];
+
+		if (empty($computers) && empty($skip)) {
+			sort($possibleClique);
+			$name = implode(',', $possibleClique);
+			$cliques[] = $name;
+		} else {
+			foreach ($computers as $c => $neigh) {
+				$newPossibleClique = array_merge($possibleClique, [$c]);
+				$newComputers = array_filter($computers, fn($x) => in_array($x, $neigh), ARRAY_FILTER_USE_KEY);
+				$newSkip = array_filter($skip, fn($x) => in_array($x, $neigh));
+
+				$cliques = array_merge($cliques, findMaximalCliques($newComputers, $newPossibleClique, $newSkip));
+
+				unset($computers[$c]);
+				$skip[] = $c;
+			}
+		}
+
+		return $cliques;
+	}
+
+	$setsOfThree = findSetsOfThree($computers);
 	$part1 = array_sum($setsOfThree);
 	echo 'Part 1: ', $part1, "\n";
 
-	// $part2 = 0;
-	// echo 'Part 2: ', $part2, "\n";
+	$part2 = '';
+	$cliques = findMaximalCliques($computers);
+	foreach ($cliques as $clique) {
+		if (strlen($clique) > strlen($part2)) {
+			$part2 = $clique;
+		}
+	}
+	echo 'Part 2: ', $part2, "\n";
