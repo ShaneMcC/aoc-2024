809558392997ac7e42835ced3380f172a3178bf2
Shane Mc Cormack <shanemcc@gmail.com>
2024-12-24 10:08:23 +0000

    Day 24. Part 2. 2879. I hate it, I hate it, I hate it.

--- 24/run.php
+++ 24/run.php
@@ -66,11 +66,108 @@
 				$result[$wire[0]] .= getWireValue($wire);
 			}
 		}
-		foreach (array_keys($result) as $k) { $result[$k] = strrev($result[$k]); }
+		foreach (array_keys($result) as $k) { $result[$k] = bindec(strrev($result[$k])); }
 
 		return $result;
 	}
 
 	$numbers = getNumbers();
 	$part1 = $numbers['z'];
-	echo 'Part 1: ', bindec($part1), "\n";
+	echo 'Part 1: ', $part1, "\n";
+
+	function getInstructionType($g) {
+		global $gates;
+
+		$gate = $gates[$g];
+
+		if ($gate['a'][0] == 'x' || $gate['a'][0] == 'y' || $gate['b'][0] == 'x' || $gate['b'][0] == 'y') {
+			if ($gate['op'] == 'XOR') {
+				return 'XYADD';
+			} else if ($gate['op'] == 'AND') {
+				return 'XYCARRY';
+			}
+		}
+
+		if ($gate['op'] == 'AND') {
+			return 'AND';
+		} else if ($gate['op'] == 'OR') {
+			return 'CARRY';
+		} else if ($gate['op'] == 'XOR') {
+			return 'ZOUT';
+		}
+
+		die("Unknown Type: {$gate['a']} {$gate['op']} {$gate['b']} -> {$g}\n");
+	}
+
+	// Making HUGE assumptions about the input.
+	// Thanks to https://www.reddit.com/r/adventofcode/comments/1hl698z/comment/m3kb5ix/
+
+	$bad = [];
+
+	$types = [];
+
+	foreach ($gates as $g => $gate) {
+		$instType = getInstructionType($g);
+		$types[$instType][] = $g;
+	}
+
+	function getGateListRegisters($gateList) {
+		global $gates;
+
+		$targets = [];
+
+		foreach ($gateList as $g) {
+			$targets[] = $gates[$g]['a'];
+			$targets[] = $gates[$g]['b'];
+		}
+
+		return $targets;
+	}
+
+	// The input is a fancy list of "Full Adders" but some things are swapped
+	// So now find any that are actually wrong.
+
+	foreach ($types['XYADD'] as $g) {
+		// Allow x00 ^ y00
+		if ($gates[$g]['a'] == 'x00' || $gates[$g]['a'] == 'y00') { continue; }
+
+		// This should never be a z and should always output to a ZOUT.
+		if ($g[0] == 'z' || !in_array($g, getGateListRegisters($types['ZOUT']))) {
+			$bad[] = $g;
+		}
+	}
+
+	foreach ($types['XYCARRY'] as $g) {
+		// Allow x00 & y00
+		if ($gates[$g]['a'] == 'x00' || $gates[$g]['a'] == 'y00') { continue; }
+
+		// This should never be a z and should always output to a CARRY.
+		if ($g[0] == 'z' || !in_array($g, getGateListRegisters($types['CARRY']))) {
+			$bad[] = $g;
+		}
+	}
+
+	// If it's not a Z, it's wrong.
+	foreach ($types['ZOUT'] as $g) {
+		if ($g[0] != 'z') {
+			$bad[] = $g;
+		}
+	}
+
+	// If it's a Z other than z45, it's wrong.
+	foreach ($types['CARRY'] as $g) {
+		if ($g[0] == 'z' && $g != 'z45') {
+			$bad[] = $g;
+		}
+	}
+
+	// If it's a Z, it's wrong
+	foreach ($types['AND'] as $g) {
+		if ($g[0] == 'z') {
+			$bad[] = $g;
+		}
+	}
+
+	sort($bad);
+	$part2 = implode(',', $bad);
+	echo 'Part 2: ', $part2, "\n";
